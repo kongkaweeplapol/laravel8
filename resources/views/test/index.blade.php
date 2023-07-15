83c8e6b19d15cdf701fe548f8f561920083e8844
kongkaweeplapol <kongkaweeplapol@gmail.com>
2023-07-15 22:59:17 +0700

    WEEK4
    WEEK4

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -9,11 +9,11 @@
     <img src="{{$god}}" alt="cat" width="200" height="200">
     <img src="{{$spider}}" alt="cat" width="200" height="200"> -->
    
-    <a href="http://localhost/laravel8/public/gallery/ant" target="_blank"><img src="{{$ant}}" alt="ant" width="200" height="200"></a>
-    <a href="http://localhost/laravel8/public/gallery/bird" target="_blank"><img src="{{$bird}}" alt="bird" width="200" height="200"></a>
-    <a href="http://localhost/laravel8/public/gallery/cat" target="_blank"><img src="{{$cat}}" alt="cat" width="200" height="200"></a>
-    <a href="http://localhost/laravel8/public/gallery/god" target="_blank"><img src="{{$god}}" alt="cat" width="200" height="200"></a>
-    <a href="http://localhost/laravel8/public/gallery/spider" target="_blank"><img src="{{$spider}}" alt="cat" width="200" height="200"></a>
+    <a href="http://localhost/laravel8/public/newgallery/ant" target="_blank"><img src="{{$ant}}" alt="ant" width="200" height="200"></a>
+    <a href="http://localhost/laravel8/public/newgallery/bird" target="_blank"><img src="{{$bird}}" alt="bird" width="200" height="200"></a>
+    <a href="http://localhost/laravel8/public/newgallery/cat" target="_blank"><img src="{{$cat}}" alt="cat" width="200" height="200"></a>
+    <a href="http://localhost/laravel8/public/newgallery/god" target="_blank"><img src="{{$god}}" alt="cat" width="200" height="200"></a>
+    <a href="http://localhost/laravel8/public/newgallery/spider" target="_blank"><img src="{{$spider}}" alt="cat" width="200" height="200"></a>
 
 </body>
 
