8ea83c046b833f358c99f2a26c7f54b262f2ae72
kongkaweeplapol <kongkaweeplapol@gmail.com>
2023-07-22 13:51:51 +0700

    WEEK5
    WEEK5

---
+++ database/migrations/2023_07_22_133931_create_product_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            //เพิ่มตาราง product
+            $table->integer('price')->nullable();
+            $table->float('total')->nullable();
+            $table->string('name')->nullable();
+            $table->date('buy_date')->nullable();
+            $table->text('description')->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
